a66b0d37beef24539c83e33e9c42dbee658ff113
unknown <ivovamez135@gmail.com>
2021-03-16 15:30:11 +0300

    HEHE

--- HTML Code/script.js
+++
@@ -1,8 +0,0 @@
-var monthly_payment; // Ежемесячные платёж
-var loan_amount; // Сумма кредита
-var monthly_interest_rate; // Ежемесячная процентная ставка
-var loan_term; // срок, на который берётся кредит
-
-monthly_payment = loan_amount * ( monthly_interest_rate + monthly_interest_rate/(pow(1 + monthly_interest_rate, loan_term) - 1) )
-
-document.write (monthly_payment) // Вывод результата
